d6d59c2c928a79420bd1452208e9dfc8f7373968
Blatoy <lock.jeanneret@gmail.com>
2019-06-23 18:14:34 +0200

    Remove dumb screen check

--- src/scripts/window-manager.js
+++ src/scripts/window-manager.js
@@ -1,8 +1,10 @@
-const { exec } = require('child_process');
+const {
+  exec
+} = require('child_process');
 const yModifier = -25;
 
 module.exports.getWindowsRectangles = () => {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     // TODO: Handle for windows  / when not installed
     exec('wmctrl -lG', (err, stdout, stderr) => {
       if (err) {
@@ -13,12 +15,15 @@ module.exports.getWindowsRectangles = () => {
       const lines = stdout.split("\n");
       let windows = [];
 
-      for(let i = 0; i < lines.length; ++i) {
+      for (let i = 0; i < lines.length; ++i) {
         let line = lines[i].replace(/[ ]{1,}/g, " ");
         let values = line.split(" ");
-        if(parseInt(values[3]) + yModifier > 0) {
-          windows.push({x: parseInt(values[2]), y: parseInt(values[3]) + yModifier, w: parseInt(values[4]), h: parseInt(values[5])});
-        }
+        windows.push({
+          x: parseInt(values[2]),
+          y: parseInt(values[3]) + yModifier,
+          w: parseInt(values[4]),
+          h: parseInt(values[5])
+        });
       }
 
       resolve(windows);
